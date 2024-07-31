80452be6ef9f4dba096e54a39f4d21cf4e35848e
ArevaloDev <arevalodaniel999@gmail.com>
2024-07-30 23:02:52 -0600

    agregando datos y haciendo comunicacion de datos entre componentes

--- RealSalesApp/src/app/home/home/home.component.ts
+++ RealSalesApp/src/app/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HoustingLocationComponent } from "../../housting-location/housting-location.component";
+import { HoustingLocation } from '../../housting-location';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,15 @@ import { HoustingLocationComponent } from "../../housting-location/housting-loca
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingLocation:HoustingLocation = {
+    id:999,
+    name:'Test Home',
+    city:'Test City',
+    state:'ST',
+    photo:`${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
 }
--- RealSalesApp/src/app/housting-location/housting-location.component.ts
+++ RealSalesApp/src/app/housting-location/housting-location.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HoustingLocation } from '../housting-location';
 
 @Component({
@@ -9,15 +9,7 @@ import { HoustingLocation } from '../housting-location';
   styleUrl: './housting-location.component.css'
 })
 export class HoustingLocationComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  housinLocation:HoustingLocation = {
-    id:999,
-    name:'Test Home',
-    city:'Test City',
-    state:'ST',
-    photo:`${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  }
+  @Input() housingLocation!:HoustingLocation;
+  
+
 }
